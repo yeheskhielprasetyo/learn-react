1ec85622b687bbc73b960bd0e5a92dd501bab5b8
yeheskhielprasetyo <yeheskhielprasetyorakordana123@gmail.com>
2023-06-14 18:51:30 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(1)
+  function click() {
+    console.log("number :", number)
+    setNumber(number + 1)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={click}>Click</button>
+        {number}
       </header>
     </div>
   );
